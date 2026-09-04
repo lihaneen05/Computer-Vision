5007c202f296d2961fbab1412f2d3eda7d051fa8
lihaneen05 <154400310+lihaneen05@users.noreply.github.com>
2026-09-04 20:38:04 +0500

    Update images.py

--- Files/images.py
+++ Files/images.py
@@ -1,7 +1,7 @@
 import cv2
 
-# Path to an image in the Images folder
-image_path = "../Images/sample1.jpg"
+# Path pointing from Files/ folder to Images/ folder
+image_path = "../Images/fall.png"
 
 # Load the image
 image = cv2.imread(image_path)
@@ -10,7 +10,7 @@ image = cv2.imread(image_path)
 if image is not None:
     # Display the image in a window
     cv2.imshow("Loaded Image", image)
-
+    
     # Wait for a key press and close the window
     cv2.waitKey(0)
     cv2.destroyAllWindows()
